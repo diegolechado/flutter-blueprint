0b069efa5eb83a6dc8957007cad7c53e4fd622e9
Diego Lechado <diegolechado@gmail.com>
2021-07-08 17:16:39 -0300

    Initial commit

---
+++ app_blueprint/android/app/src/main/kotlin/com/example/app_blueprint/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_blueprint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_blueprint/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BlueprintApp());
+}
+
+class BlueprintApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Blueprint app'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("Home"),
+      ),
+    );
+  }
+}
