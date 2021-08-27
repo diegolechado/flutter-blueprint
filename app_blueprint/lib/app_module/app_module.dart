841c3c5fa7b64ee79334e5efadf1580978ae59b5
Raphael Bertachine do Rego <raphael.zeppelim@gmail.com>
2021-08-27 10:27:14 -0300

    Add custom splash screen

--- app_blueprint/lib/app_module/app_module.dart
+++ app_blueprint/lib/app_module/app_module.dart
@@ -1,15 +1,15 @@
-import 'package:app_blueprint/app_module/blocs/pulls/pulls_bloc.dart';
-import 'package:app_blueprint/app_module/repositories/pulls_repository.dart';
-import 'package:app_blueprint/app_module/repositories/user_repository.dart';
-import 'package:app_blueprint/app_module/datasource/connect_datasource.dart';
-import 'package:app_blueprint/app_module/datasource/storage_datasource.dart';
-import 'package:app_blueprint/app_module/screens/pulls_screen.dart';
-import 'package:app_blueprint/app_module/use_case/pulls_usecase.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'blocs/pulls/pulls_bloc.dart';
+import 'blocs/home/home_bloc.dart';
+import 'repositories/pulls_repository.dart';
+import 'repositories/user_repository.dart';
+import 'datasource/connect_datasource.dart';
+import 'datasource/storage_datasource.dart';
 import 'datasource/user_datasource.dart';
 import 'use_case/user_repositories_usecase.dart';
-import 'blocs/home/home_bloc.dart';
+import 'use_case/pulls_usecase.dart';
 import 'screens/home_screen.dart';
+import 'screens/pulls_screen.dart';
 import 'screens/settings_screen.dart';
 
 class AppModule extends Module {
